af721b68bf084499f0724dcaa2d3c03bff5c35fd
haoching918 <haoching918@gmail.com>
2023-04-27 21:59:15 +0800

    now support multithread

--- discretize.py
+++ discretize.py
@@ -1,8 +1,10 @@
 import json
+from tqdm import tqdm, trange
+from concurrent.futures import ThreadPoolExecutor
 import datetime as dt
 
 def getScale(hour):
-    return str(hour // 3)
+    return str(hour // timeScale)
 
 def initDeviceData():
     # history per airbox
@@ -49,27 +51,12 @@ def fillAvg(deviceWeek):
             time['avgTemperature'] = total / len(time['temperature']) if len(time['temperature']) != 0 else 0
     return deviceWeek
 
-# set cur time         
-with open('./info.json', 'r') as f:
-    info = json.load(f)
-endDatetime = dt.datetime.strptime(info['update_datetime'][:13], '%Y-%m-%d %H') # current date time without min and sec
-startDatetime = endDatetime - dt.timedelta(days=7)
-# get time scale
-timeScale = info['time_scale']
-
-# get all device id
-with open('./data/all_device_id.json','r') as f:
-    devicesIdJson = json.load(f)
-devicesId = devicesIdJson['id']
-
-allDeviceWeek = [] # store discretized device data of a week in discretized_device_week
-
-cnt = 1
-for i in devicesId:
-    with open(f"./data/device_week/{i}.json", 'r') as f:
+# store discretized data of a given device in discretized_device_week
+def store_device_week(id):
+    with open(f"./data/device_week/{id}.json", 'r') as f:
         weekData = json.load(f)
     if weekData['num_of_records'] == 0:
-        continue
+        return
     deviceWeek = initDeviceData()
     deviceWeek['deviceId'] = weekData['device_id']
     o =  list(weekData['feeds'][0]['AirBox'][0].values())[0]
@@ -89,37 +76,60 @@ for i in devicesId:
         deviceWeek['data'][str(deviceDatetime.date())][getScale(deviceDatetime.hour)]['temperature'].append(data['s_t0'])
 
     fillAvg(deviceWeek)
-    allDeviceWeek.append(deviceWeek)
-    
+    # allDeviceWeek.append(deviceWeek)   
     jsonData = json.dumps(deviceWeek)
-    with open(f'./data/discretized_device_week/{i}.json', 'w') as f:
+    with open(f'./data/discretized_device_week/{id}.json', 'w') as f:
         f.write(jsonData)
-    print(f'{cnt} ./data/discretized_device_week/{i}.json saved')
-    cnt += 1
+    return deviceWeek
 
-# store all discretized device data of certain time scale in time_week
-for i in range(24 // timeScale):
-    for j in range(24 // timeScale):
-        curDatetime = (startDatetime + dt.timedelta(days=i)).date()
-        timeWeek = {
-                'timeStamp': str(curDatetime),
-                'timeScale' : j,
-                'data' : []                
-            }
-        for d in allDeviceWeek:
-            o = d['data'][str(curDatetime)][str(j)]
-            data = {
-                'deviceId' : d['deviceId'],
-                'siteName' : d['siteName'],
-                'gps_lat' : d['gps_lat'],
-                'gps_lon' : d['gps_lon'],
-                'pm2.5' : o['avgPm2.5'],
-                'humidity' : o['avgHumidity'],
-                'temperature' : o['avgTemperature']
-            }
-            timeWeek['data'].append(data)
-        
-        jsonData = json.dumps(timeWeek)
-        with open(f'./data/time_week/{curDatetime}-{j}.json', 'w') as f:
-            f.write(jsonData)
-        print(f'{i*7+j} ./data/time_week/{curDatetime}-{j}.json saved')
+# store all discretized device data of certain day and time in time_week
+def store_date(day, time):
+    curDatetime = (startDatetime + dt.timedelta(days=day)).date()
+    timeWeek = {
+            'timeStamp': str(curDatetime),
+            'timeScale' : time,
+            'data' : []                
+        }
+    for d in allDeviceWeek:
+        o = d['data'][str(curDatetime)][str(time)]
+        data = {
+            'deviceId' : d['deviceId'],
+            'siteName' : d['siteName'],
+            'gps_lat' : d['gps_lat'],
+            'gps_lon' : d['gps_lon'],
+            'pm2.5' : o['avgPm2.5'],
+            'humidity' : o['avgHumidity'],
+            'temperature' : o['avgTemperature']
+        }
+        timeWeek['data'].append(data)
+    
+    jsonData = json.dumps(timeWeek)
+    with open(f'./data/time_week/{curDatetime}-{time}.json', 'w') as f:
+        f.write(jsonData)
+         
+# set cur time         
+with open('./info.json', 'r') as f:
+    info = json.load(f)
+endDatetime = dt.datetime.strptime(info['update_datetime'][:13], '%Y-%m-%d %H') # current date time without min and sec
+startDatetime = endDatetime - dt.timedelta(days=7)
+# get time scale
+timeScale = info['time_scale']
+
+# get all device id
+with open('./data/all_device_id.json','r') as f:
+    devicesIdJson = json.load(f)
+devicesId = devicesIdJson['id']
+
+allDeviceWeek = [] # store discretized device data of a week in discretized_device_week
+
+print("storing discretized device week data")
+with ThreadPoolExecutor(max_workers=10) as executor:
+    for r in tqdm(executor.map(store_device_week, devicesId), total=len(devicesId)):
+        allDeviceWeek.append(r)
+
+print("storing time week data")
+for day in trange(8):
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        times = range(24//timeScale)
+        days = [day for _ in times]
+        executor.map(store_date, days, times)
