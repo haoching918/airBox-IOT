010074cb5d80f4f293b804e7c6c8cae73ca2d039
haoching918 <haoching918@gmail.com>
2023-04-27 22:21:36 +0800

    add wind directory

--- init_dir.py
+++ init_dir.py
@@ -1,11 +1,18 @@
 import os
 
 if not os.path.exists("./data"):
+    print("init ./data")
     os.makedirs("./data")
 if not os.path.exists("./data/device_week"):
+    print("init ./data/device_week")
     os.makedirs("./data/device_week")
 if not os.path.exists("./data/discretized_device_week"):
+    print("init ./data/discretized_device_week")
     os.makedirs("./data/discretized_device_week")
 if not os.path.exists("./data/time_week"):
+    print("init ./data/time_week")
     os.makedirs("./data/time_week")
+if not os.path.exists("./data/wind"):
+    print("./data/wind")
+    os.makedirs("./data/wind")
     
